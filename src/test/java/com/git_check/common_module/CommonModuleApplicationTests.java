acc4d74789c03f0d7056b2585aded9cd8daf8998
leebokyeong <kyeong8139@gmail.com>
2025-08-02 11:45:28 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/git_check/common_module/CommonModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_check.common_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
